ed89d282f72507870c037177ee7b206c14b86b5b
Viacava <viacava@outlook.com>
2015-08-19 18:59:14 -0300

    cambios

---
+++ application/views/public/includes/index/wealcome_message.php
@@ -0,0 +1,9 @@
+<?php
+// Pagina de inicio de la aplicacion ademas es la pagina por defecto.
+defined('BASEPATH') or exit('No direct script access allowed');
+?>
+<div class="container">
+
+<p>Hola</p>
+
+</div>
---
+++ application/views/public/includes/sing_up/message_sing_up.php
@@ -0,0 +1,10 @@
+
+<div class="col-sm-10 col-sm-push-1">
+	<div class="panel panel-primary">
+		<div class="panel-heading">
+			<h3 class="panel-title">Usuario registrado</h3>
+		</div>
+		<div class="panel-body">The requested page has been permanently moved
+			to a new location.</div>
+	</div>
+</div>
