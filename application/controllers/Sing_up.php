9291170feb6bcc8330b4125c656310d28dc70f3e
Viacava <viacava@outlook.com>
2015-08-22 18:19:28 -0300

    cambio de pagina de registro y eliminación de pagina ultra demo

--- application/controllers/Sing_up.php
+++ application/controllers/Sing_up.php
@@ -3,26 +3,6 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Welcome extends CI_Controller
 {
-
-    /**
-     * Index Page for this controller.
-     *
-     * Maps to the following URL
-     * http://example.com/index.php/welcome
-     * - or -
-     * http://example.com/index.php/welcome/index
-     * - or -
-     * Since this controller is set as the default controller in
-     * config/routes.php, it's displayed at http://example.com/
-     *
-     * So any other public methods not prefixed with an underscore will
-     * map to /index.php/welcome/<method_name>
-     *
-     * @see http://codeigniter.com/user_guide/general/urls.html public function index()
-     *      {
-     *      $this->load->view('welcome_message');
-     *      }
-     */
     function __construct()
     {
         parent::__construct();
@@ -42,7 +22,7 @@ class Welcome extends CI_Controller
         $this->lang->load($languages_files);
     }
 
-    public function index()
+    public function sing_up_page()
     {
         $this->form_validation->set_rules('name', 'Nombre', 'trim|required|min_length[2]|max_length[255]');
         $this->form_validation->set_rules('lastname', 'Apellido', 'trim|required|min_length[2]|max_length[255]');
@@ -68,4 +48,3 @@ class Welcome extends CI_Controller
         $this->load->view("public/page_element/footer");
     }
 }
-
