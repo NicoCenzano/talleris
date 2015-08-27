f98193c3457930ece35328fc74d2a3ec2fda8571
Viacava <viacava@outlook.com>
2015-08-26 22:24:03 -0300

    cambio mysql

--- application/controllers/Sing_up.php
+++ application/controllers/Sing_up.php
@@ -41,12 +41,7 @@ class Sing_up extends CI_Controller
             $this->load->view("public/includes/sing_up/message_sing_up.php");
             $this->load->view("public/includes/sing_up/access_form");
             
-            for($i=100007; $i<=1500000; $i++){
-                
-                $this->User_model->_add_user($i, $i, $i*rand(1,1000), $i."@kode.cl");
-            }
-            
-            //$this->User_model->_add_user($this->input->post('name'), $this->input->post('lastname'), $this->input->post('password'), $this->input->post('email'));
+            $this->User_model->_add_user($this->input->post('name'), $this->input->post('lastname'), $this->input->post('password'), $this->input->post('email'));
        
         } else {
             $this->load->view("public/includes/sing_up/access_form");
