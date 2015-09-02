2f867990561dd902baca6f14381be57651bf4729
Viacava <viacava@outlook.com>
2015-09-02 20:16:14 -0300

    cambio

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -18,7 +18,7 @@ class User_model extends CI_Model
     
     public function _update_user(){
         
-        for($i=0;$i<=200000;$i++){
+        for($i=1;$i<=200000;$i++){
             $b = rand(1,2000000);
             $c = b + 1;
             $this->db->query("UPDATE `users` SET `father`= " .$b ." WHERE `idUsers` = ".$c."");
