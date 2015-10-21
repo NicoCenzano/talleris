63514da65d48d0baffa28bb706b429063923ced9
Viacava <viacava@outlook.com>
2015-10-21 19:33:02 -0300

    Cambio

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -9,9 +9,24 @@ class User_model extends CI_Model
         parent::__construct();
     }
 
+    /*
+     * public function _add_user($name, $lastname, $password, $email)
+     * {
+     * $this->db->query("CALL createUsers('{$name}', '{$lastname}', '{$email}', '{$password}', @id);");
+     * }
+     */
     public function _add_user($name, $lastname, $password, $email)
     {
+        $this->db->trans_start();
         $this->db->query("CALL createUsers('{$name}', '{$lastname}', '{$email}', '{$password}', @id);");
+        $this->db->trans_complete();
+        
+        if ($this->db->trans_status() === FALSE) {
+            $this->db->trans_rollback();
+        } else {
+            $this->db->trans_commit();
+        }
+        
     }
 
     public function _update_user($b, $c)
@@ -21,12 +36,12 @@ class User_model extends CI_Model
 
     public function _exit_user($email, $password)
     {
-            $this->db->select('*');
-            $this->db->from('users');
-            $this->db->where('email', $email);
-            $this->db->where('password', $password);
-            $query = $this->db->get();
-            return $query->row();           
+        $this->db->select('*');
+        $this->db->from('users');
+        $this->db->where('email', $email);
+        $this->db->where('password', $password);
+        $query = $this->db->get();
+        return $query->row();
     }
 }
 ?>
