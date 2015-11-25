2f15ec4751becd82ed3b7597b5c079bb10528a14
Viacava <viacava@outlook.com>
2015-11-25 16:03:31 -0300

    estetica

--- application/views/public/includes/welcome/wealcome_message.php
+++ application/views/public/includes/welcome/wealcome_message.php
@@ -3,12 +3,12 @@
 defined('BASEPATH') or exit('No direct script access allowed');
 ?>
 
-
-<div class="panel panel-primary">
-	<div class="panel-heading">
-		<h3 class="panel-title">Proyecto</h3>
-	</div>
-	<div class="panel-body">
-		Proyecto Taller de ingenieria en software año 2015
+<div class="col-sm-8 col-sm-push-2">
+	<div class="panel panel-primary">
+		<div class="panel-heading">
+			<h3 class="panel-title">Proyecto</h3>
+		</div>
+		<div class="panel-body">Proyecto Taller de ingenieria en software año
+			2015</div>
 	</div>
 </div>
